d564da4009866afd3bae87d97d014c81b35fa591
L1m1tlesAyman <limtgojo@gmail.com>
2026-07-25 01:44:10 +0200

    Feat: Add Services Layer

---
+++ app/Services/UserServices.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
+
+class UserServices{
+    public function insertUser(array $validatedInfo ,?UploadedFile $image):User{
+        $imagePath = null;
+        if($image){
+            $imagePath = $image->store('photos','public');
+        }
+        $user = User::create([
+            'name' => $validatedInfo['name'],
+            'email' => $validatedInfo['email'],
+            'password'=> Hash::make($validatedInfo['password']),
+            'image' => $imagePath
+        ]);
+
+        return $user;
+    }
+
+    public function UpdateUser(array $validatedInfo , User $user , ?UploadedFile $image)
+    {
+        if(empty($validatedInfo['password'])){
+            unset($validatedInfo['password']);
+        }else{
+            $validatedInfo['password'] = Hash::make($validatedInfo['password']);
+        }
+        if($image){
+            if($user->image){
+                Storage::disk('public')->delete($user->image);
+            }
+            $validatedInfo['image'] = $image->store('photos', 'public');
+        }
+        $user->update($validatedInfo);
+
+        return $user;
+    }
+}
