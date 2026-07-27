6eaf25b4f7d18f19d9f75d86e631ab46e6c892ae
L1m1tlesAyman <limtgojo@gmail.com>
2026-07-27 22:50:53 +0200

    Update Columns Name

--- database/migrations/2026_07_27_172238_report_table.php
+++ database/migrations/2026_07_27_172238_report_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         //
         Schema::create('reports',function(Blueprint $table){
             $table->id();
-            $table->string('reason');
+            $table->string('raison');
             $table->morphs('reportable');
             $table->foreignId('reporter_id')->constrained('users')->cascadeOnDelete();
             $table->timestamps();
