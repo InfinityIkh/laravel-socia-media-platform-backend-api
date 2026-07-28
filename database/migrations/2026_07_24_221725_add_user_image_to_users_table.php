4b5e05b19b661829e1e9c57234e79f50e0b726ff
L1m1tlesAyman <limtgojo@gmail.com>
2026-07-28 23:59:11 +0200

    Update path column to have a default value

--- database/migrations/2026_07_24_221725_add_user_image_to_users_table.php
+++ database/migrations/2026_07_24_221725_add_user_image_to_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
-            $table->string('path');
+            $table->string('path')->default('public/profile/deafult.png');
         });
     }
 
