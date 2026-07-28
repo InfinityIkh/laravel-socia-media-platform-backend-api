7cb36fa8300ade7c6b6cf22046bcd6c36c9c7f91
L1m1tlesAyman <limtgojo@gmail.com>
2026-07-28 22:29:02 +0200

    Feat: Add SoftDeletes trait to User,Post and Comment models

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,9 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comment extends Model
 {
+    use SoftDeletes;
     protected $fillable = [
         'body',
         'user_id',
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,9 +8,11 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+    use SoftDeletes;
     protected $fillable = [
         'title',
         'body',
