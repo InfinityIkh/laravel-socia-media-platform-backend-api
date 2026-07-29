114a82d4ce7e71e86eb928fa45024afd5a299da8
L1m1tlesAyman <limtgojo@gmail.com>
2026-07-29 21:29:33 +0200

    Feat: Add Follow Requests relationship

--- app/Models/FollowRequest.php
+++ app/Models/FollowRequest.php
@@ -3,9 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class FollowRequest extends Model
 {
     //
-    protected $guarded = [];
+    protected $table = 'follow_requests';
+    protected $fillable = [
+        'sender_id',
+        'receiver_id',
+        'status'
+    ];
+
+    public function sender():BelongsTo{
+        return $this->belongsTo(User::class,'sender_id');
+    }
+
+    public function receiver():BelongsTo{
+        return $this->belongsTo(User::class,'receiver_id');
+    }
 }
