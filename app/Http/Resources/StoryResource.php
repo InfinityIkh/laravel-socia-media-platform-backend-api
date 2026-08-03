93dc618e1d30fdb5a704d66ca0cbe3675140f8f4
L1m1tlesAyman <limtgojo@gmail.com>
2026-08-03 23:05:38 +0200

    Feat: Add Story Resource

---
+++ app/Http/Resources/StoryResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class StoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'media_path' => $this->media_path,
+            'media_type' => $this->media_type,
+            'expires_at' => $this->expires_at,
+            'author' => $this->user()
+        ];
+    }
+}
