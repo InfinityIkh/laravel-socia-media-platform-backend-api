fd56fdd378662f7e4538a39dae82e77e5bc23fcd
L1m1tlesAyman <limtgojo@gmail.com>
2026-08-05 01:18:19 +0200

    Feat: Update Story Controller by Add Crud Operation And more functionality

--- app/Http/Controllers/StoryController.php
+++ app/Http/Controllers/StoryController.php
@@ -12,7 +12,27 @@ use Illuminate\Support\Facades\Storage;
 class StoryController extends Controller
 {
     //
-    public function uploadStory(StoryServices $storyService ,StoryRequest $request){
+    public function storyViewers(Story $story){
+        //
+        $this->authorize('view',$story);
+        $users = $story->with('users')->get();
+
+        return response()->json([
+            'users' => StoryResource::collection($users)
+        ],200);
+    }
+
+    public function index(Request $request){
+        //
+        $currentUser = $request->user();
+        $stories = Story::with('user','users')->visible($currentUser)->get();
+
+        return response()->json([
+            'stories' => StoryResource::collection($stories)
+        ],200);
+    }
+
+    public function store(StoryServices $storyService ,StoryRequest $request){
         //
         $currentUser = $request->user();
         $media = $request->file('media');
@@ -20,11 +40,36 @@ class StoryController extends Controller
         $story = $storyService->uploadStory($currentUser ,$media);
 
         return response()->json([
-            'story' => new StoryResource($story->load('user'))
+            'story' => new StoryResource($story->load('user','users'))
         ],201);
     }
 
-    public function removeStory(Story $story){
+    //public function update(StoryServices $storyService ,StoryRequest $request ,Story $story){
+    //    //
+    //    $this->authorize('update',$story);
+    //    $media = $request->file('media');
+    //    $validated = $request->validated();
+//
+    //    $story = $storyService->updateStory($story ,$validated ,$media);
+//
+    //    return response()->json([
+    //        'story' => new StoryResource($story->load('user','users'))
+    //    ],200);
+    //}
+
+    public function show(Request $request ,Story $story){
+        //
+        $currentUser = $request->user();
+        $this->authorize('poepleCanView',$story);
+
+        $story->users()->syncWithoutDetaching($currentUser->id);
+
+        return response()->json([
+            'story' => new StoryResource($story->load('user'))
+        ],200);
+    }
+
+    public function destroy(Story $story){
         //
         $this->authorize('delete',$story);
 
