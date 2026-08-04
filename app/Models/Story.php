c02de6640b631048df324f567c615e4ae37b5df5
L1m1tlesAyman <limtgojo@gmail.com>
2026-08-04 23:59:18 +0200

    Feat: Add new Relationshep in model

--- app/Models/Story.php
+++ app/Models/Story.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Story extends Model
 {
@@ -14,5 +15,10 @@ class Story extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class,'story_views');
     }
 }
