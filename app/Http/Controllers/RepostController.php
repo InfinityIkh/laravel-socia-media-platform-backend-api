d046c69f4d8f1378ee8e1dbbcd1ac1b09fc347cd
L1m1tlesAyman <limtgojo@gmail.com>
2026-08-06 23:46:19 +0200

    refactor: use Redis to improve repost functionality performance

--- app/Http/Controllers/RepostController.php
+++ app/Http/Controllers/RepostController.php
@@ -5,30 +5,40 @@ namespace App\Http\Controllers;
 use App\Events\UserRepostedEvent;
 use App\Http\Resources\UserResource;
 use App\Models\Post;
+use App\Models\User;
+use App\Services\PostService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class RepostController extends Controller
 {
     //
-    public function repost(Request $request , Post $post){
+    public function repost(PostService $postService ,Request $request , Post $post){
         //
+        $currentUser = $request->user();
         $this->authorize('view',$post);
-        $repost = $post->reposts()->toggle($request->user()->id);
-        if(!empty($repost['attached'])){
-            event(new UserRepostedEvent($request->user() , $post));
-        }
-        $message = !empty($repost['attached']) ? 'Post reposted successfully.' : 'Post removed from reposted posts.' ;
+
+        $repost = $postService->repostPost($currentUser ,$post);
+
         return response()->json([
-            'message' => $message
+            'message' => $repost ? 'Post reposted successfully.' : 'Post removed from reposted posts.'
         ],200);
     }
 
-    public function repostsUsers(Request $request ,Post $post){
+    public function repostsUsers(Post $post){
         //
-        $repostsUsers = $post->visible($request->user())->load('reposts');
+        $$usersDb = $post->reposts()->get();
+
+        $key = 'post:'.$post->id.':reposts';
+        $usersIds = Redis::sMembers($key);
+        $redisUsers = User::whereIn('id' ,$usersIds)->get();
+
+        $allUsers = $usersDb->merge($redisUsers)
+                            ->unique('id')
+                            ->values();
 
         return response()->json([
-            'reposted_users' => UserResource::collection($repostsUsers->reposts)
+            'reposted_users' => UserResource::collection($allUsers)
         ],200);
     }
 }
