398711842490433de1e4d4507b674438f6e27e99
L1m1tlesAyman <limtgojo@gmail.com>
2026-08-06 23:40:10 +0200

    Feat: Add Caching by Redis for Feed Posts

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -5,18 +5,24 @@ namespace App\Http\Controllers;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class FeedController extends Controller
 {
     //
     public function index(Request $request){
         //
-        $followingIds = $request->user()->following->pluck('users.id');
-        $followingPosts = Post::with(['user'])
-                                ->visible($request->user())
+        $currentUser = $request->user();
+        
+        $followingPosts = Cache::remember('user:'.$currentUser->id.':following:posts' ,3600 ,function() use($currentUser){
+            $followingIds = $currentUser->following()->pluck('users.id');
+            $posts = Post::with(['user'])
+                                ->visible($currentUser)
                                 ->whereIn('user_id',$followingIds)
                                 ->latest()
                                 ->get();
+            return $posts;
+        });
 
         return response()->json([
             'posts' => PostResource::collection($followingPosts)
@@ -24,11 +30,15 @@ class FeedController extends Controller
     }
 
     public function mostLiked(Request $request){
-        $posts = Post::with(['user','likes'])
-                       ->visible($request->user())
+        $currentUser = $request->user();
+        $posts = Cache::remember('user:'.$currentUser->id.':following:posts:mostLiked',3600 ,function()use($currentUser){
+            $posts = Post::with(['user','likes'])
+                       ->visible($currentUser)
                        ->withCount('likes')
                        ->orderBy('likes_count','desc')
                        ->get();
+            return $posts;
+        });
                        
         return response()->json([
             'posts' => PostResource::collection($posts)
@@ -36,11 +46,15 @@ class FeedController extends Controller
     }
 
     public function mostWatched(Request $request){
-        $posts = Post::with(['user','views'])
-                       ->visible($request->user())
+        $currentUser = $request->user();
+        $posts = Cache::remember('user:'.$currentUser->id.':following:posts:mostWatched',3600 ,function()use($currentUser){
+            $posts = Post::with(['user','views'])
+                       ->visible($currentUser)
                        ->withCount('views')
                        ->orderBy('views_count' , 'desc')
                        ->get();
+            return $posts;
+        });
 
         return response()->json([
             'posts' => PostResource::collection($posts)
