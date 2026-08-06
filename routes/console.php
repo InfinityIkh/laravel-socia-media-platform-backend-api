fb7a9e5eeff22f264987f6aee2edaf25d7691b1c
L1m1tlesAyman <limtgojo@gmail.com>
2026-08-07 00:10:32 +0200

    feat: add Laravel scheduler for Redis sync jobs

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Jobs\CountPostLikesJob;
+use App\Jobs\CountPostRepostsJob;
+use App\Jobs\CountPostSavesJob;
+use App\Jobs\CountPostViewsJob;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::job(new CountPostViewsJob)->everyTwoHours();
+Schedule::job(new CountPostLikesJob)->everyTwoHours();
+Schedule::job(new CountPostRepostsJob)->everyTwoHours();
+Schedule::job(new CountPostSavesJob)->everyTwoHours();
