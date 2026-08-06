bfe16dd2df8b67177e0879236b72d811742d0942
L1m1tlesAyman <limtgojo@gmail.com>
2026-08-07 00:08:44 +0200

    feat: extract business logic into service layer

--- app/Services/PostServices.php
+++ app/Services/PostServices.php
@@ -2,10 +2,14 @@
 
 namespace App\Services;
 
+use App\Events\UserLikedEvent;
 use App\Events\UserMentionEvent;
+use App\Jobs\SendLikeNotificationJob;
+use App\Jobs\SendRepostNotificationJob;
 use App\Models\HashTag;
 use App\Models\Post;
 use App\Models\User;
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Storage;
 
 class PostService{
@@ -100,4 +104,56 @@ class PostService{
         }
         $post->images()->delete();
     }
+
+    public function likePost(User $currentUser ,Post $post){
+        //
+        $likesKey = 'post:'.$post->id.':likes';
+        $unlikesKey = 'post:'.$post->id.':unlikes';
+        $likedUser = $post->likes()->where('user_id',$currentUser->id)->exists() || Redis::sismember($likesKey , $currentUser->id);
+        if($likedUser){
+            Redis::sAdd($unlikesKey ,$currentUser->id);
+            Redis::srem($likesKey ,$currentUser->id);
+            $like = false;
+        }else{
+            Redis::sAdd($likesKey ,$currentUser->id);
+            Redis::srem($unlikesKey ,$currentUser->id);
+            $like = true;
+            SendLikeNotificationJob::dispatch($currentUser ,$post);
+        }
+        return $like;
+    }
+
+    public function savePost(User $currentUser ,Post $post){
+        $savedKey = 'post:'.$post->id.':saves';
+        $unsavedKey = 'post:'.$post->id.':unsaves';
+        $userSave = $post->saves()->where('user_id',$currentUser->id)->exists() || Redis::sismember($savedKey ,$currentUser->id);
+        if($userSave){
+            Redis::sAdd($unsavedKey ,$currentUser->id);
+            Redis::srem($savedKey ,$currentUser->id);
+            $save = false;
+        }else{
+            Redis::sAdd($savedKey ,$currentUser->id);
+            Redis::srem($unsavedKey ,$currentUser->id);
+            $save = true;
+        }
+        return $save;
+    }
+
+    public function repostPost(User $currentUser ,Post $post){
+        $repostKey = 'post:'.$post->id.':reposts';
+        $unrepostKey = 'post:'.$post->id.':unreposts';
+        $userSave = $post->reposts()->where('user_id',$currentUser->id)->exists() || Redis::sismember($repostKey ,$currentUser->id);
+        if($userSave){
+            Redis::sAdd($unrepostKey ,$currentUser->id);
+            Redis::srem($repostKey ,$currentUser->id);
+            $repost = false;
+        }else{
+            Redis::sAdd($repostKey ,$currentUser->id);
+            Redis::srem($unrepostKey ,$currentUser->id);
+            $repost = true;
+            SendRepostNotificationJob::dispatch($currentUser ,$post);
+        }
+        return $repost;
+    }
+
 }
