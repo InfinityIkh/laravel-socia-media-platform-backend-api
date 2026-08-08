845099084360e965704d0852418d9d934bfbc190
L1m1tlesAyman <limtgojo@gmail.com>
2026-08-08 23:33:47 +0200

    Feat: Add Queue Job for processing images

---
+++ app/Jobs/ProcessPostImagesJob.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Post;
+use App\Services\ProcessMediaService;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Storage;
+
+class ProcessPostImagesJob implements ShouldQueue
+{
+    use Queueable;
+    public $tries = 3;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(public Post $post ,public string $path)
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        //
+        $oldImages = $this->post->images()->get();
+
+        $ProcessMediaServices = new ProcessMediaService();
+        $outputImage = $ProcessMediaServices->processImage($this->path);
+
+        $this->post->images()->create([
+            'image_path' => $outputImage
+        ]);
+
+            foreach($oldImages as $image){
+                Storage::disk('public')->delete($image->image_path);
+            }
+            $oldImages->each->delete();
+    }
+}
---
+++ app/Jobs/ProcessUserImagesJob.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\User;
+use App\Services\ProcessMediaService;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Storage;
+
+class ProcessUserImagesJob implements ShouldQueue
+{
+    use Queueable;
+    public $tries = 3;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(public User $user ,public string $path)
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        //
+        $oldImage = $this->user->path;
+
+        $ProcessMediaServices = new ProcessMediaService();
+        $outputImage = $ProcessMediaServices->processImage($this->path);
+
+        $this->user->update([
+            'path' => $outputImage
+        ]);
+
+        if($outputImage){
+            Storage::disk('public')->delete($oldImage);
+        }
+    }
+}
