469adef8eb2768a130c7d7692ed357a65c3f6418
L1m1tlesAyman <limtgojo@gmail.com>
2026-08-10 23:29:14 +0200

    Refactor: PostImages model and remove timetsamps storing value

--- app/Models/PostImages.php
+++ app/Models/PostImages.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class PostImages extends Model
 {
     //
+    public $timestamps = false;
     protected $fillable = [
         'post_id',
         'image_path'
