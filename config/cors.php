729d60f707ba5cbc533da15044f39ba520ea4bfc
L1m1tlesAyman <limtgojo@gmail.com>
2026-08-13 23:25:12 +0200

    refactor: update CORS to allow broadcasting authentication

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie','login','logout'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie','broadcasting/auth','login','logout'],
 
     'allowed_methods' => ['*'],
 
