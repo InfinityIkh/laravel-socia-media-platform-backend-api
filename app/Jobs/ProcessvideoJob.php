54e3b12d1cd65e6ee6e13d6b7258053fd182315c
L1m1tlesAyman <limtgojo@gmail.com>
2026-08-16 23:56:47 +0200

    feat: add video proccesing queue

---
+++ app/Jobs/ProcessvideoJob.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Story;
+use App\Services\ProcessMediaService;
+use App\Services\StoryServices;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+
+class ProcessvideoJob implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(public Story $story ,public string $path)
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(ProcessMediaService $processMediaService): void
+    {
+        //
+        $path = $processMediaService->processVideo($this->path ,$this->story->media_type);
+
+        $this->story->update([
+            'media_type' => $path
+        ]);
+    }
+}
