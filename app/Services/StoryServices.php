3f390fab658a794b841db37a295ce194e9eec43f
L1m1tlesAyman <limtgojo@gmail.com>
2026-08-18 21:42:28 +0200

    feat: add new story services

--- app/Services/StoryServices.php
+++ app/Services/StoryServices.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Jobs\ProcessStoryImageJob;
 use App\Jobs\ProcessvideoJob;
 use App\Models\Story;
 use App\Models\User;
@@ -9,7 +10,37 @@ use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 
 class StoryServices{
-    //
+    public function uploadStory(User $user , UploadedFile $file): Story
+    {
+        //
+        $media_path = null;
+        $media_type = null;
+        $media_type = str_starts_with($file->getMimeType() ,'image/') ? 'image' : 'video';
+        $media_path = match($media_type){
+            'image' => $file->store('stories/image/original','public'),
+            'video' => $file->store('stories/video/original','public')
+        };
+
+        $story = Story::create([
+            'user_id' => $user->id,
+            'media_path' => $media_path,
+            'media_type' => $media_type,
+            'expires_at' => now()->addDay()
+        ]);
+
+        $story->media_type === 'image' ? ProcessStoryImageJob::dispatch($story ,$media_path) : ProcessvideoJob::dispatch($story ,$media_path);
+
+        return $story;
+    }
+
+    public function deleteExpiredStories(){
+        Story::where('expires_at' ,'<=' ,now())->chunkById(100 ,function($stories){
+            $paths = $stories->pluck('media_path')->filter()->all();
+            Storage::disk('public')->delete($paths);
+            $stories->each->delete();
+        });
+    }
+        //
     //public function updateStory(Story $story ,array $validated , ?UploadedFile $file): Story
     //{
     //    //
@@ -22,26 +53,4 @@ class StoryServices{
 //
     //    return $story;
     //}
-    public function uploadStory(User $user , UploadedFile $file): Story
-    {
-        //
-        $media_path = null;
-        $media_type = null;
-        $media_type = str_starts_with($file->getMimeType() ,'image/') ? 'image' : 'video';
-        $media_path = match($media_type){
-            'image' => $file->store('images/stories/images/original','public'),
-            'vedio' => $file->store('images/stories/videos/original','public')
-        };
-
-        $story = Story::create([
-            'user_id' => $user->id,
-            'media_path' => $media_path,
-            'media_type' => $media_type,
-            'expires_at' => now()->addDay()
-        ]);
-
-        $story->media_type === 'image' ? '' : ProcessvideoJob::dispatch($story ,$media_path);
-
-        return $story;
-    }
 }
