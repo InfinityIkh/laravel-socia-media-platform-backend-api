8d4d87171acee0cfc2ea1538b433ace17bb2764c
L1m1tlesAyman <limtgojo@gmail.com>
2026-08-18 21:35:15 +0200

    feat: add scheduler to delete expired stories

--- routes/console.php
+++ routes/console.php
@@ -4,6 +4,7 @@ use App\Jobs\CountPostLikesJob;
 use App\Jobs\CountPostRepostsJob;
 use App\Jobs\CountPostSavesJob;
 use App\Jobs\CountPostViewsJob;
+use App\Jobs\DeleteExpiredStoriesJob;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
@@ -12,7 +13,9 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::job(new CountPostViewsJob)->everyTwoHours();
-Schedule::job(new CountPostLikesJob)->everyTwoHours();
-Schedule::job(new CountPostRepostsJob)->everyTwoHours();
-Schedule::job(new CountPostSavesJob)->everyTwoHours();
+Schedule::job(new CountPostViewsJob)->everyTenMinutes();
+Schedule::job(new CountPostLikesJob)->everyTenMinutes();
+Schedule::job(new CountPostRepostsJob)->everyTenMinutes();
+Schedule::job(new CountPostSavesJob)->everyTenMinutes();
+
+Schedule::job(new DeleteExpiredStoriesJob)->everyMinute();
