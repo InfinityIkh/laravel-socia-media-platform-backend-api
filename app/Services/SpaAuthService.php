fd13390dd18af7806d769c9c94cd0e97ff0494dd
L1m1tlesAyman <limtgojo@gmail.com>
2026-08-26 21:37:06 +0200

    feat: add SPA auth service

--- app/Services/SpaAuthService.php
+++ app/Services/SpaAuthService.php
@@ -2,7 +2,44 @@
 
 namespace App\Services;
 
-class SpaAuthService
-{
+use App\Http\Requests\UserRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
 
+class SpaAuthService implements AuthStrategyInterface
+{
+    //
+    public function login(UserRequest $request): JsonResponse
+    {
+        //
+        $credentials = $request->validated();
+        if (!Auth::attempt([
+            'email' => $credentials['email'],
+            'password' => $credentials['password'],
+        ])) {
+            return response()->json([
+                'message' => 'Invalid credentials.'
+            ], 401);
+        }
+
+        $request->session()->regenerate();
+
+        return response()->json([
+            'message' => 'Login successful.',
+            'user' => $request->user(),
+        ]);
+    }
+
+    public function logout(UserRequest $request): JsonResponse
+    {
+        //
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return response()->json([
+            'message' => 'Logout successful.'
+        ]);
+    }
 }
