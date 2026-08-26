9bb8888ef523db23ff093e7f96bd567707ffc6dc
L1m1tlesAyman <limtgojo@gmail.com>
2026-08-26 21:44:39 +0200

    feat: add authentication strategy resolver

--- app/Services/AuthResolver.php
+++ app/Services/AuthResolver.php
@@ -2,7 +2,19 @@
 
 namespace App\Services;
 
+use InvalidArgumentException;
+
 class AuthResolver
 {
-
+    //
+    public function resolve(string $type): AuthStrategyInterface
+    {
+        //
+        $class = match($type){
+            'token' => TokenAuthService::class,
+            'spa' => SpaAuthService::class,
+            default => throw new InvalidArgumentException("Unsupported authentication type: {$type}")
+        };
+        return app($class);
+    }
 }
