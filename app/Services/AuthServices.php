802980711be397735a6f0d5321c78266c5a56899
L1m1tlesAyman <limtgojo@gmail.com>
2026-08-26 21:46:58 +0200

    feat: add authentication services

--- app/Services/AuthServices.php
+++ app/Services/AuthServices.php
@@ -2,7 +2,25 @@
 
 namespace App\Services;
 
+use App\Http\Requests\UserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Hash;
+
 class AuthServices
 {
-
+    //
+    public function register(array $credentials): JsonResponse
+    {
+        $user = User::create([
+            'name' => $credentials['name'],
+            'email' => $credentials['email'],
+            'password'=> Hash::make($credentials['password']),
+            'role' => $credentials['role'] ?? 'user'
+        ]);
+        return response()->json([
+            'user' => new UserResource($user),
+        ]);
+    }
 }
