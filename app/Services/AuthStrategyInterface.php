9f79a1ad2d4d0edcb1d1ffd4d877933ff3150aab
L1m1tlesAyman <limtgojo@gmail.com>
2026-08-26 21:40:44 +0200

    feat: add authentication strategy interface

--- app/Services/AuthStrategyInterface.php
+++ app/Services/AuthStrategyInterface.php
@@ -2,7 +2,12 @@
 
 namespace App\Services;
 
+use App\Http\Requests\UserRequest;
+
 interface AuthStrategyInterface
 {
+    //
+    public function login(UserRequest $request);
 
+    public function logout(UserRequest $request);
 }
