ba6dcf5f4af78e9235d0c26dd728f86432845c0e
L1m1tlesAyman <limtgojo@gmail.com>
2026-08-26 21:37:56 +0200

    feat: add Tokens auth service

--- app/Services/TokenAuthService.php
+++ app/Services/TokenAuthService.php
@@ -2,7 +2,43 @@
 
 namespace App\Services;
 
-class TokenAuthService
+use App\Http\Requests\UserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Hash;
+
+class TokenAuthService implements AuthStrategyInterface
 {
+    //
+    public function login(UserRequest $request): JsonResponse
+    {
+        //
+        $credentials = $request->validated();
+        $user = User::where('email', $credentials['email'])->first();
+        if (!$user ||!Hash::check($credentials['password'], $user->password))
+        {
+            return response()->json([
+                'message' => 'Invalid credentials.'
+            ], 401);
+        }
+
+        $token = $user->createToken('auth-token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Login successful.',
+            'user' => new UserResource($user),
+            'token' => $token,
+        ]);
+    }
+
+    public function logout(UserRequest $request): JsonResponse
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json([
+            'message' => 'Logout successful.'
+        ]);
+    }
 
 }
