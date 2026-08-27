e35cac896f456849e719e7aac21b9c6c6a22e478
L1m1tlesAyman <limtgojo@gmail.com>
2026-08-27 23:33:14 +0200

    feat: add JWT auth strategy

--- app/Services/JwtAuthService.php
+++ app/Services/JwtAuthService.php
@@ -2,7 +2,30 @@
 
 namespace App\Services;
 
-class JwtAuthService
-{
+use Illuminate\Validation\ValidationException;
 
+class JwtAuthService implements AuthStrategyInterface
+{
+    //
+    public function login(array $credentials): array
+    {
+        //
+        if (!$token = auth('api')->attempt($credentials)) {
+            throw ValidationException::withMessages([
+                'message' => ['Invalid credentials.'],
+            ]);
+        }
+
+        return [
+            'access_token' => $token,
+            'token_type'   => 'bearer',
+            'expires_in'   => auth('api')->factory()->getTTL() * 60,
+        ];
+    }
+
+    public function logout(): void
+    {
+        //
+        auth('api')->logout();
+    }
 }
