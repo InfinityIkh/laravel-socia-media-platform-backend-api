fda54c93dc1ac71b558e8993713c54a27583976e
L1m1tlesAyman <limtgojo@gmail.com>
2026-08-28 03:59:49 +0200

    refactor: update the auth controller methods

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,26 +5,34 @@ namespace App\Http\Controllers;
 use App\Http\Requests\UserRequest;
 use App\Services\AuthResolver;
 use App\Services\AuthServices;
+use Illuminate\Http\JsonResponse;
 
 class AuthController extends Controller
 {
     public function __construct(public AuthResolver $resolver){}
 
-    public function register(AuthServices $authServices, UserRequest $request){
+    public function register(AuthServices $authServices, UserRequest $request): JsonResponse
+    {
         //
         $credentials = $request->validated();
         return $authServices->register($credentials);
     }
 
-    public function login(UserRequest $request ,string $type){
+    public function login(UserRequest $request ,string $type): JsonResponse
+    {
         //
+        $credentials = $request->validated();
         $resolver = $this->resolver->resolve($type);
-        return $resolver->login($request);
+        return response()->json($resolver->login($credentials));
     }
 
-    public function logout(UserRequest $request ,string $type){
+    public function logout(string $type): JsonResponse
+    {
         //
         $resolver = $this->resolver->resolve($type);
-        return $resolver->logout($request);
+        $resolver->logout();
+        return response()->json([
+            'message' => 'Successfully logged out'
+        ]);
     }
 }
